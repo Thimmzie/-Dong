426e1563b95ec39dd487ba203d8847236bd0b247
Ayodeji <thimmzieayodeji@gmail.com>
2024-12-16 19:32:55 +0100

    worked on mobile hamburger

---
+++ main.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navBar = document.querySelector(".site-nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navBar.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navBar.classList.remove("active");
+  })
+);
