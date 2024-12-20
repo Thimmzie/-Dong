59dd32ef7e412e875703dbec77a8e739e0d29270
Ayodeji <thimmzieayodeji@gmail.com>
2024-12-20 07:41:32 +0100

    modified the mobile screen

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//this code is for the hamburger menu
 const hamburger = document.querySelector(".hamburger");
 const navBar = document.querySelector(".site-nav");
 
@@ -12,3 +13,33 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navBar.classList.remove("active");
   })
 );
+
+//this code is for the copy button
+document.querySelectorAll(".copy-btn").forEach((copyButton) => {
+  copyButton.addEventListener("click", () => {
+    const targetEelement = document.querySelector(copyButton.dataset.copy);
+    const textToCopy = targetEelement.textContent.replace(/\s+/g, "").trim();
+
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      const label = copyButton.querySelector(".copy-label");
+      const originalText = label.textContent;
+
+      copyButton.disabled = true;
+      label.textContent = "Copied!";
+
+      setTimeout(() => {
+        copyButton.disabled = false;
+        label.textContent = originalText;
+      }, 1000);
+    });
+  });
+});
+
+//this code is for the faq page
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  });
+});
