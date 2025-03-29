a9208d6f6226811682cf0017ff4756926962aad4
Ayodeji <thimmzieayodeji@gmail.com>
2025-03-29 15:33:16 +0100

    mobile ui

--- wallet.js
+++ wallet.js
@@ -1107,7 +1107,7 @@ window.addEventListener("resize", () => {
   if (!modal || !wrapper) return;
 
   if (window.innerHeight < screen.height * 0.9) {
-    modal.style.top = "120%";
+    modal.style.top = "80%";
     document.body.style.overflow = "hidden";
     wrapper.style.overflow = "hidden";
   } else {
